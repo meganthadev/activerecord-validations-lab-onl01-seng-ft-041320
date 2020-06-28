fca51281568167837676a9040f0936e089cd0a8b
Megan Horsfield <meganthadev@gmail.com>
2020-06-28 04:51:00 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   
   validates :title, presence: true 
-  validates :post_content, length: { minimum: 250 }
+  validates :content, length: { minimum: 250 }
   
   
 end
