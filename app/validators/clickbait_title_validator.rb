71736a5d9a06c87d0874084f0c8c18ea1b0e9b87
Megan Horsfield <meganthadev@gmail.com>
2020-06-28 05:06:47 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_title_validator.rb
+++ app/validators/clickbait_title_validator.rb
@@ -1,4 +1,5 @@
 class ClickbaitTitleValidator < ActiveModel::EachValidator
+  
   def validate_each(record, attribute, value)
     unless value =~ /\b(Won't Believe|Secret|Top \d|Guess)\b/
       record.errors[attribute] << "Title is not clickbaity enough"
