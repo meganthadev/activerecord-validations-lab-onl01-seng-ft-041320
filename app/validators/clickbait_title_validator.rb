4e8a4503692d5184d31fe4f8694d353eba5a58f0
Megan Horsfield <meganthadev@gmail.com>
2020-06-28 05:14:18 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_title_validator.rb
+++ app/validators/clickbait_title_validator.rb
@@ -1,4 +1,4 @@
-class ClickbaitTitleValidator < ActiveModel::EachValidator
+class ClickbaitTitleValidator < ActiveModel::Validator
   
   def validate(record)
     unless record.title.include?("Won't Believe", "Secret", "Top [number]", "Guess")
