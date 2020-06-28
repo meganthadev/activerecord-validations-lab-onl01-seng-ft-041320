6aa175f30b71de05aea41d14374acb8ffa00f911
Megan Horsfield <meganthadev@gmail.com>
2020-06-28 05:14:02 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_title_validator.rb
+++ app/validators/clickbait_title_validator.rb
@@ -1,8 +1,8 @@
 class ClickbaitTitleValidator < ActiveModel::EachValidator
   
   def validate(record)
-    unless record.title.include?("Won't Believe", "Secret", "Top [number]", or "Guess)
-      record.errors[:email] << "We're only allowed to have people who work for the company in the database!"
+    unless record.title.include?("Won't Believe", "Secret", "Top [number]", "Guess")
+      record.errors[:email] << "Need clickbait!"
     end
   end
 end
