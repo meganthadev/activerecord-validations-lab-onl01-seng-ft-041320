c3acec29bbc6a69994277746e311939849c8d9c3
Megan Horsfield <meganthadev@gmail.com>
2020-06-28 05:10:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,8 @@
 class Post < ActiveRecord::Base
   
-  validates :title, presence: true 
+  validates :title, presence: true, clickbait_title: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validates_with ClickbaitTitleValidator, on: :create
-  
+
 end
