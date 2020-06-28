98351b2b74e3e576ab9787f69ab05ac663da761f
Megan Horsfield <meganthadev@gmail.com>
2020-06-28 05:18:37 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_title_validator.rb
+++
@@ -1,8 +0,0 @@
-class ClickbaitTitleValidator < ActiveModel::Validator
-  
-  def validate(record)
-    unless record.title.include?("Won't Believe", "Secret", "Top [number]", "Guess")
-      record.errors[:email] << "Need clickbait!"
-    end
-  end
-end
